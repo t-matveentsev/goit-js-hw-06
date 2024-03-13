cfcdee151b0235cd07a54038b3a5248e31656909
t-matveentsev <tmatveentsev@gmail.com>
2024-03-13 16:28:42 +0100

    update_hw

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,32 @@
 'use strict';
 
-const objC = { c: "objC prop" };
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  }, 
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
 
-const objB = Object.create(objC);
-objB.b = "objB prop";
-
-const objA = Object.create(objB);
-objA.a = "objA prop";
-
-console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
-console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
-console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
